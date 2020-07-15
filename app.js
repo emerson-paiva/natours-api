d30fbbf59216fdf07470ff768761c6d399a9768d
Emerson Paiva <emersonaraujopaiva@gmail.com>
2020-07-14 22:07:23 -0300

    chore: add morgan logger

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
 const express = require('express');
-const { get } = require('http');
+const morgan = require('morgan');
 
 const app = express();
 
+app.use(morgan('dev'));
 app.use(express.json());
 
 const toursFilePath = `${__dirname}/dev-data/data/tours-simple.json`;
