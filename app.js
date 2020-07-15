a6bdecbc5ac1087ed6cc1f234a73d82d39c755b2
Emerson Paiva <emersonaraujopaiva@gmail.com>
2020-07-14 21:14:52 -0300

    feat: add tours first endpoints

--- app.js
+++ app.js
@@ -1,10 +1,53 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.use(express.json());
+
+const toursFilePath = `${__dirname}/dev-data/data/tours-simple.json`;
+const tours = JSON.parse(fs.readFileSync(toursFilePath));
+
+app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
-    message: 'Hello from the server side!',
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  const { params } = req;
+  const tour = tours.find(({ id }) => id === Number(params.id));
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'failure',
+      message: 'not-found',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: { tour },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const { body } = req;
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = { id: newId, ...body };
+
+  tours.push(newTour);
+  fs.writeFile(toursFilePath, JSON.stringify(tours), () => {
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
   });
 });
 
