acc33832ee51d5ad248eab05eb348a27ef0649fe
Emerson Paiva <emersonaraujopaiva@gmail.com>
2020-07-20 21:42:58 -0300

    chore: add env variables

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const tourRouter = require('./routes/tour.routes');
 const userRouter = require('./routes/user.routes');
 
 const app = express();
-
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
