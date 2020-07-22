de929fe2dc17c5c8f5ae08a504c7924756cd7c84
Emerson Paiva <emersonaraujopaiva@gmail.com>
2020-07-21 22:47:06 -0300

    feat: add delete route to tour

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -12,8 +12,8 @@ const getAllTours = async (_, res) => {
       },
     });
   } catch (error) {
-    res.status(500).json({
-      status: 'failure',
+    res.status(404).json({
+      status: 'fail',
       message: error,
     });
   }
@@ -31,8 +31,8 @@ const getTourById = async ({ params }, res) => {
       },
     });
   } catch (error) {
-    res.status(500).json({
-      status: 'failure',
+    res.status(404).json({
+      status: 'fail',
       message: error,
     });
   }
@@ -59,7 +59,28 @@ const createTour = async ({ body }, res) => {
 const updateTour = async ({ params, body }, res) => {
   try {
     const { id } = params;
-    const tour = await Tour.findByIdAndUpdate(id, body);
+    const tour = await Tour.findByIdAndUpdate(id, body, {
+      new: true,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'failure',
+      message: error,
+    });
+  }
+};
+
+const deleteTour = async ({ params }, res) => {
+  try {
+    const { id } = params;
+    const tour = await Tour.findByIdAndDelete(id);
 
     res.status(200).json({
       status: 'success',
@@ -80,4 +101,5 @@ module.exports = {
   getTourById,
   createTour,
   updateTour,
+  deleteTour,
 };
--- routes/tour.routes.js
+++ routes/tour.routes.js
@@ -4,11 +4,12 @@ const {
   getTourById,
   createTour,
   updateTour,
+  deleteTour,
 } = require('../controllers/tourController');
 
 const router = express.Router();
 
 router.route('/').get(getAllTours).post(createTour);
-router.route('/:id').get(getTourById).patch(updateTour);
+router.route('/:id').get(getTourById).patch(updateTour).delete(deleteTour);
 
 module.exports = router;
