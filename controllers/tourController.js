9a6bd8cbd72aa02a2a50e91f96ca84564bca9f82
Emerson Paiva <emersonaraujopaiva@gmail.com>
2020-07-28 22:30:52 -0300

    feat: add paginations and fields to tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,7 +1,17 @@
 const Tour = require('../models/tourModel');
 
+const aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAvegare,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
+
 const getAllTours = async (req, res) => {
   try {
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 10;
+    const skip = (page - 1) * limit;
     const queryParams = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryParams[el]);
@@ -16,6 +26,21 @@ const getAllTours = async (req, res) => {
       query = query.sort(sortBy);
     } else {
       query = query.sort('-createdAt');
+    }
+
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+
+      if (skip >= numTours) throw new Error('This page does not exist');
     }
 
     const tours = await query;
@@ -117,4 +142,5 @@ module.exports = {
   createTour,
   updateTour,
   deleteTour,
+  aliasTopTours,
 };
